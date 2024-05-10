6c33b36a8ca2c47772f974eeff9c600be368d089
mhasanmeet <mhasan.meet@gmail.com>
2024-05-10 17:30:43 +0600

    method chaining

---
+++ 111-method-chaining.php
@@ -0,0 +1,22 @@
+<?php 
+
+class chaining{
+    public function first(){
+        echo "This is first function \n";
+        return $this;
+    }
+
+    public function second(){
+        echo "This is second function \n";
+        return $this;
+    }
+
+    public function third(){
+        echo "This is third function \n";
+        return $this;
+    }
+}
+
+
+$chainingObj = new chaining();
+$chainingObj->first()->second()->third();
